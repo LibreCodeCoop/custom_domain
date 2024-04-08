7fe86e1e48019dd9c217ed35003839fe11a9280d
Vitor Mattos <vitor@php.rio>
2024-04-07 21:15:39 -0300

    Make possible customize default color
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -26,11 +26,50 @@ declare(strict_types=1);
 
 namespace OCA\CustomDomain\AppInfo;
 
+use OC\AppConfig;
+use OC_Defaults;
 use OCA\CustomDomain\Middleware\InjectionMiddleware;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
+
+class CustomDomainAppConfig extends AppConfig {
+	private array $inMemory = [];
+	public function setInMemory($key, $value) {
+		$this->inMemory[$key] = $value;
+	}
+
+	public function getValue($app, $key, $default = null) {
+		if ($app === 'theming') {
+			return $this->inMemory[$key] ?? parent::getValue($app, $key, $default);
+		}
+		return parent::getValue($app, $key, $default);
+	}
+}
+
+class ConfigOverwriteNC28 extends \OC\Config {
+	/** @var string[] */
+	private array $overWrite = [];
+
+	public function __construct(
+		string $configDir,
+	) {
+		parent::__construct($configDir);
+	}
+
+	public function getValue($key, $default = null) {
+		if (isset($this->overWrite) && isset($this->overWrite[$key])) {
+			return $this->overWrite[$key];
+		}
+
+		return parent::getValue($key, $default);
+	}
+
+	public function setValue($key, $value) {
+		$this->overWrite[$key] = $value;
+	}
+}
 
 /**
  * @codeCoverageIgnore
@@ -43,6 +82,42 @@ class Application extends App implements IBootstrap {
 	}
 
 	public function boot(IBootContext $context): void {
+		$this->overrideDefaultColor();
+	}
+
+	private function overrideDefaultColor() {
+		$service = $this->getConfig();
+		$defaults = \OC::$server->get(OC_Defaults::class);
+		$reflection = new \ReflectionClass($defaults);
+		$property = $reflection->getProperty('color');
+		$property->setAccessible(true);
+		$color = $property->getValue($defaults);
+		$service->setInMemory('color', $color);
+	}
+
+	private function getConfig() {
+		if (\OCP\Util::getVersion() < '29') {
+			$service = \OC::$server->get(\OCP\IConfig::class);
+			if (!$service instanceof ConfigOverwriteNC28) {
+				\OC::$server->registerService(\OCP\IConfig::class, function () {
+					return new ConfigOverwriteNC28(\OC::$configDir);
+				});
+				$service = \OC::$server->get(\OCP\IConfig::class);
+			}
+		} else {
+			$service = \OC::$server->get(\OC\AppConfig::class);
+			if (!$service instanceof CustomDomainAppConfig) {
+				\OC::$server->registerService(\OC\AppConfig::class, function () {
+					return new CustomDomainAppConfig(
+						\OC::$server->get(\OCP\IDBConnection::class),
+						\OC::$server->get(\Psr\Log\LoggerInterface::class),
+						\OC::$server->get(\OCP\Security\ICrypto::class),
+					);
+				});
+				$service = \OC::$server->get(\OC\AppConfig::class);
+			}
+		}
+		return $service;
 	}
 
 	public function register(IRegistrationContext $context): void {
